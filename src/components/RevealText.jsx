2fd6dbaaed66485ea6d5bca1bcd788dc82f7927d
Muhammad Umaid Rather <107248121+humiiii@users.noreply.github.com>
2025-06-14 13:21:00 +0530

    completed hero section and added final animation to heading text

--- src/components/RevealText.jsx
+++ src/components/RevealText.jsx
@@ -1,15 +1,78 @@
-import React from "react";
+import { useRef } from "react";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-const RevealText = ({
-  field,
+gsap.registerPlugin(useGSAP, ScrollTrigger);
+
+export const RevealText = ({
+  text = "",
   id,
   align = "start",
-  as = "div",
+  as: Component = "div",
   duration = 0.8,
   className,
   staggerAmount = 0.1,
+  triggerStart = "top 80%",
+  triggerEnd = "bottom 20%",
 }) => {
-  return <div>RevealText</div>;
-};
+  const componentRef = useRef(null);
 
-export default RevealText;
+  const words = text.split(" ");
+
+  useGSAP(
+    () => {
+      const mm = gsap.matchMedia();
+
+      mm.add("(prefers-reduced-motion: no-preference)", () => {
+        gsap.to(".reveal-text-word", {
+          y: 0,
+          stagger: staggerAmount,
+          duration,
+          ease: "power3.out",
+          scrollTrigger: {
+            trigger: componentRef.current,
+            start: triggerStart,
+            end: triggerEnd,
+          },
+        });
+      });
+
+      mm.add("(prefers-reduced-motion: reduce)", () => {
+        gsap.to(".reveal-text-word", {
+          duration: 0.5,
+          opacity: 1,
+          ease: "none",
+          y: 0,
+          stagger: 0,
+        });
+      });
+    },
+    { scope: componentRef },
+  );
+
+  return (
+    <Component
+      className={`reveal-text text-balance ${
+        align === "center"
+          ? "text-center"
+          : align === "end"
+            ? "text-right"
+            : "text-left"
+      } ${className || ""}`}
+      ref={componentRef}
+    >
+      {words.map((word, index) => (
+        <span
+          key={`${word}-${index}-${id}`}
+          className="mb-0 inline-block overflow-hidden pb-4"
+        >
+          <span className="reveal-text-word mt-0 inline-block translate-y-[120%] will-change-transform">
+            {word}
+            {index < words.length - 1 ? <>&nbsp;</> : null}
+          </span>
+        </span>
+      ))}
+    </Component>
+  );
+};
--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { Bounded } from "../components/Bounded";
 import Button from "../components/Button";
 import FadeIn from "../components/FadeIn";
+import { RevealText } from "../components/RevealText";
 
 const Hero = () => {
+  const [imgLoaded, setImgLoaded] = useState(false);
+
   return (
     <Bounded className="relative min-h-screen overflow-hidden bg-neutral-950">
       <FadeIn
@@ -13,31 +16,39 @@ const Hero = () => {
         <img
           src="/cote-royale/images/hero.jpg"
           className="absolute top-0 left-0 object-cover"
+          loading="lazy"
+          onLoad={() => setImgLoaded(true)}
         />
       </FadeIn>
-
-      <div className="relative flex h-screen flex-col justify-center">
-        <h1 className="font-gambarino max-w-xl text-6xl leading-tight text-neutral-50 md:text-7xl lg:text-8xl">
-          Effortless Elegance
-        </h1>
-        <FadeIn
-          vars={{ delay: 1, duration: 1.3 }}
-          className="font-raleway mt-6 max-w-md translate-y-8 text-lg text-neutral-50"
-        >
-          An expression of quiet luxury, Côte Royale is designed for the man who
-          commands attention without seeking it. A reflection of nature’s raw
-          beauty.
-        </FadeIn>
-        <FadeIn
-          vars={{ delay: 1.7, duration: 1.1 }}
-          className={"translate-y-5"}
-        >
-          <Button
-            text={"shop now"}
-            styles={"border border-white text-white hover:bg-white/20"}
+      {imgLoaded && (
+        <div className="relative flex h-screen flex-col justify-center">
+          <RevealText
+            text={"Effortless Elegance"}
+            id={"hero-heading"}
+            staggerAmount={0.3}
+            duration={2}
+            as="h1"
+            className="font-gambarino max-w-xl text-6xl leading-[.8] text-neutral-50 md:text-7xl lg:text-8xl"
           />
-        </FadeIn>
-      </div>
+          <FadeIn
+            vars={{ delay: 1, duration: 1.3 }}
+            className="font-raleway mt-6 max-w-md translate-y-8 text-lg text-neutral-50"
+          >
+            An expression of quiet luxury, Côte Royale is designed for the man
+            who commands attention without seeking it. A reflection of nature’s
+            raw beauty.
+          </FadeIn>
+          <FadeIn
+            vars={{ delay: 1.7, duration: 1.1 }}
+            className={"translate-y-5"}
+          >
+            <Button
+              text={"shop now"}
+              styles={"border border-white text-white hover:bg-white/20"}
+            />
+          </FadeIn>
+        </div>
+      )}
     </Bounded>
   );
 };
